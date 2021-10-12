f5b06c4fa651dd17fbf2479409794dabd70a4120
jangbigom91 <jangbigom91@gmail.com>
2021-10-12 15:22:38 +0900

    fetching movies from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import axios from 'axios';
 
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  }
+  
+  // axios는 기다려야 됨
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
   }
   
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading : false});
-    }, 6000); 
+    this.getMovies();
   }
   
   render () {
