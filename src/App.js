651f63cfdd1de214511191b657ecc3f74a9c09cd
jangbigom91 <jangbigom91@gmail.com>
2021-10-12 12:16:43 +0900

    plan movie component

--- src/App.js
+++ src/App.js
@@ -3,40 +3,19 @@ import PropTypes from "prop-types";
 
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component {
-  
-  // state 안의 데이터들은 변함
   state = {
-    count: 0
-  };
-  
-  // setState를 호출할 때마다 react는 새로운 state와 함께 render function을 호출
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
-  // component Mount
-  componentDidMount() {
-    console.log("component rendered");
+    isLoading: true
   }
   
-  // component Update
-  componentDidUpdate() {
-    console.log("component updated");
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading : false});
+    }, 6000); 
   }
-
-  render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+  
+  render () {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading...": "We are ready"}</div>;
   }
 }
 
