2a6ace2048980dc57f7f3ac7ada298f939ca5ce3
jangbigom91 <jangbigom91@gmail.com>
2021-10-12 10:13:41 +0900

    reactjs prop-types

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 4.8
   },
   
   {
     id: 2,
     name: "Samgyeopsal",
-    image: "https://static.hubzum.zumst.com/hubzum/2021/03/12/08/d53c3c3a7dd54125b3b3235a6882a26e.jpg"
+    image: "https://static.hubzum.zumst.com/hubzum/2021/03/12/08/d53c3c3a7dd54125b3b3235a6882a26e.jpg",
+    rating: 4.9
   },
   
   {
     id: 3,
     name: "Bibimbap",
-    image: "http://bonworld.co.kr/./files/attach/images/183/306/e97a24481c4332ff4bc9f8ce88bb4224.jpg"
+    image: "http://bonworld.co.kr/./files/attach/images/183/306/e97a24481c4332ff4bc9f8ce88bb4224.jpg",
+    rating: 5
   },
   
   {
     id: 4,
     name: "Doncasu",
-    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.7
   },
 
   {
     id: 5,
     name: "Kimbap",
-    image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    image: "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 5
   },
     
   {
     id: 6,
     name: "Samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 4.5
   }
 ]
 
-function Food({ name, picture }){
+function Food({ name, picture, rating }){
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src= {picture} alt={name} />
   </div>
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image}/>)}
+      {foodILike.map(dish => 
+        (<Food 
+            key={dish.id} 
+            name={dish.name} 
+            picture={dish.image} 
+            rating={dish.rating}
+          />
+      ))}
     </div>
   );
 }
