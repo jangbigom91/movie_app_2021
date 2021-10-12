d6ff9e05d53c47dbbf52f91e8c1560e100f632b4
jangbigom91 <jangbigom91@gmail.com>
2021-10-12 16:35:10 +0900

    rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import axios from 'axios';
+import Movie from "./Movie";
 
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component {
@@ -11,7 +12,8 @@ class App extends React.Component {
   
   // axios는 기다려야 됨
   getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {data : {data: { movies }}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
   
   componentDidMount(){
@@ -19,8 +21,23 @@ class App extends React.Component {
   }
   
   render () {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading...": "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading...": movies.map(movie => {
+          return (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+              />
+          );
+        })}
+      </div>
+    );
   }
 }
 
